be79695ed53a142ec3f0ae803c016015eca7ed9d
alex <alexander.grablevski@gmail.com>
2020-07-22 15:16:43 +0300

    Add http exporter

---
+++ config.py
@@ -0,0 +1,6 @@
+import os
+
+LKL_HOST = os.getenv('LKL_HOST', 'http://86.57.193.146:50052')
+CH_HOST = os.getenv('CH_HOST', 'localhost')
+START_BLOCK = int(os.getenv('START_BLOCK', '0'))
+WAIT_BLOCK_POLICY = os.getenv('WAIT_BLOCK_POLICY', 'wait')
